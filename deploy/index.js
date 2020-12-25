8466d6a7608a74f743e766eae5189291347a4481
倩 <1670644339@qq.com>
2020-12-25 14:50:51 +0800

    0

--- deploy/index.js
+++ deploy/index.js
@@ -2,19 +2,21 @@ const scpClient = require('scp2');
 const chalk = require('chalk');
 const server = require('./products');
 const Client = require('ssh2').Client;
-const conn = new Client();
 const ora = require('ora');
 var compressing = require("compressing");
 const environment = server.path+server.localpath
 const spinner = ora('正在发布到' + environment + '服务器...');
 const ziping = ora('正在压缩' + environment);
+
+const conn = new Client();
+
 //压缩
 ziping.start()
 compressing.zip.compressDir( server.localpath + "/", "dist.zip")
   .then((e) => {
-    console.log(chalk.green('压缩成功'));
     ziping.stop()
-    spinner.start();
+    console.log(chalk.green('Success! 压缩成功'));
+    spinner.start()
     scpClient.scp(
       'dist.zip',
       {
@@ -31,10 +33,13 @@ compressing.zip.compressDir( server.localpath + "/", "dist.zip")
           throw err;
         } else {
           conn.on('ready', () => {
-            conn.exec('cd '+server.path+' \nrm -rf '+server.localpath+'\nunzip -o dist.zip', (err, stream) => {
+            conn.exec('sudo unzip -o '+server.path+'dist.zip\ndocker restart nginx', (err, stream) => {
               if (err) throw err;
-              console.log(chalk.green('Success! 成功发布到' + environment + '服务器! \n'));
-              conn.end()
+              stream
+              .on('close', function(code, signal) {
+                console.log(chalk.green('Success! 成功发布到' + environment + '服务器! \n'));
+                conn.end()
+              })
             })
           }).connect({
             host: server.host,
@@ -48,5 +53,5 @@ compressing.zip.compressDir( server.localpath + "/", "dist.zip")
     );
   })
   .catch(err => {
-    console.error('zip', err);
+    console.log(chalk.red('Fail! 压缩失败'));
   });
